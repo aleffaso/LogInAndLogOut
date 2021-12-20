dce541a7ddbe016fcb5fb80efe7f850408558cab
Aleff <aleff.a.s.o@gmail.com>
2021-12-20 18:59:46 -0300

    My first login page

---
+++ config/routes.js
@@ -0,0 +1,68 @@
+const express = require("express")
+const routes = express.Router()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+
+const initializePassport = require('./passport')
+initializePassport(
+    passport, 
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
+
+const users = []
+
+routes.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', {name: req.user.name })
+})
+
+routes.get('/login',checkNotAuthenticated,  (req, res) => {
+    res.render('login.ejs')
+})
+
+routes.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+routes.get('/register', checkNotAuthenticated, (req,res) => {
+    res.render('register.ejs')
+})
+
+routes.post('/register', checkNotAuthenticated, async (req,res) => {
+    try {
+         const hashedPassword = await bcrypt.hash(req.body.password, 10)
+         users.push({
+             id: Date.now().toString(),
+             name: req.body.name,
+             email: req.body.email,
+             password: hashedPassword
+         })
+         res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+    console.log(users)
+})
+
+routes.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if(req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
+
+module.exports = routes;
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const passport = require('passport')
+const flash = require ('express-flash')
+const session = require ('express-session')
+const dotenv = require('dotenv')
+const methodOverride = require('method-override')
+const routes = require("./config/routes"); // organize routes
+
+dotenv.config({path: './.env'});
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET, 
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+app.use(routes)
+
+app.listen(3000, () => {
+    console.log("Server Running")
+})
